9788c4d3d8e353b0e7321d13c7dc99aa24977a5b
alex ghose <alex.ghose@generalassemb.ly>
2020-10-28 21:00:40 -0400

    add memoization

--- naive-knapsack/naive-knapsack.cpp
+++ naive-knapsack/naive-knapsack.cpp
@@ -3,15 +3,22 @@
 #include <algorithm>
 using namespace std;
 
+// assume 0 <= i <= 100, and 0 <= capacity <= 100
+vector<vector<int>> cache(100, std::vector<int>(100, -1)); // memoization we store the max value for a given i and current_capacity.
+
 // return max value of selected items. w = weights, v = values, c = remaining capacity/maxweight. i = current item
 int getMaxValue(vector<int>& weights, vector<int>& values, int capacity, int i) {
 	if (i == weights.size() || capacity <= 0) return 0;
+	if (cache[i][capacity] > -1) {
+		return cache[i][capacity];
+	}
 
 	if (weights[i] < capacity) { // can include
-		return std::max(values[i] + getMaxValue(weights, values, capacity - weights[i], i+1),
-			getMaxValue(weights, values, capacity, i+1));
+		cache[i][capacity] = std::max(values[i] + getMaxValue(weights, values, capacity - weights[i], i+1),getMaxValue(weights, values, capacity, i+1));
+		return cache[i][capacity];
 	} else {
-		return getMaxValue(weights, values, capacity, i + 1);
+		cache[i][capacity] = getMaxValue(weights, values, capacity, i + 1);
+		return cache[i][capacity];
 	}
 }
 
